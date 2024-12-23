229f39b0a87a8d92b675d547fb620f7e457a567f
Grigoriy Ksyonz <KsyonzGS@gmail.com>
2024-12-23 21:34:17 +0300

    feat: add three solutions

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -21,8 +21,8 @@
  *  0  => true
  *  -5 => false
  */
-function isPositive(/* number */) {
-  throw new Error('Not implemented');
+function isPositive(number) {
+  return number >= 0;
 }
 
 /**
@@ -38,8 +38,11 @@ function isPositive(/* number */) {
  *  -5, 0, 5      => 5
  *  -0.1, 0, 0.2  => 0.2
  */
-function getMaxNumber(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getMaxNumber(a, b, c) {
+  let max = a;
+  if (max < b) max = b;
+  if (max < c) max = c;
+  return max;
 }
 
 /**
@@ -60,8 +63,16 @@ function getMaxNumber(/* a, b, c */) {
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  */
-function canQueenCaptureKing(/* queen, king */) {
-  throw new Error('Not implemented');
+function canQueenCaptureKing(queen, king) {
+  if (queen.x === king.x) return true;
+  if (queen.y === king.y) return true;
+  for (let i = 1; i < 8; i += 1) {
+    if (queen.x + i === king.x && queen.y + i === king.y) return true;
+    if (queen.x + i === king.x && queen.y - i === king.y) return true;
+    if (queen.x - i === king.x && queen.y + i === king.y) return true;
+    if (queen.x - i === king.x && queen.y - i === king.y) return true;
+  }
+  return false;
 }
 
 /**
@@ -82,8 +93,8 @@ function canQueenCaptureKing(/* queen, king */) {
  *  2, 2, 5   => false
  *  3, 0, 3   => false
  */
-function isIsoscelesTriangle(/* a, b, c */) {
-  throw new Error('Not implemented');
+function isIsoscelesTriangle(a, b, c) {
+  
 }
 
 /**
